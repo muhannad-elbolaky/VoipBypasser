44aaf4b3ff50677530352820a6ee3badbaa813f6
MajorAmari <muhannadexz@gmail.com>
2022-02-05 08:14:11 +0200

    first commit

---
+++ VoipBypasser.ps1
@@ -0,0 +1,67 @@
+Set-PSDebug -Off
+Write-host ""
+Write-host ""
+Write-host "(--> Getting things done... please wait!! <--)"
+Write-host ""
+Write-host ""
+
+# ? Global Variables
+$Settings = Get-Content -Path $pwd"\settings.json" | ConvertFrom-Json
+$vpnName = $Settings.vpnName
+$vpnServerAddress = $Settings.ServerAddress
+$vpnUserName = $Settings.Username
+$vpnPassword = $Settings.Password
+
+# ? Get hostname
+if (!$vpnServerAddress) { 
+    Write-host ""
+    Write-host ""
+    Write-host "You have to define a vpn hostname to continue"
+    $Settings.ServerAddress = Read-Host 'please visit https://www.vpngate.net/ and get one...'
+    Write-host "(Right-click or CTRL+V to paste)"
+    Write-host ""
+    Write-host ""
+    $vpnServerAddress = $Settings.ServerAddress
+    $Settings | ConvertTo-Json -depth 100 | Out-File $pwd"\settings.json"
+}
+
+function Connect-VPN {
+    $cmd = $env:WINDIR + "\System32\rasdial.exe"
+    $expression = "$cmd $vpnName $vpnUserName $vpnPassword"
+    Invoke-Expression -Command $expression -erroraction silentlycontinue
+}
+
+function Disconnect-VPN {
+    $cmd = $env:WINDIR + "\System32\rasdial.exe"
+    $expression = "$cmd $vpnName /disconnect"
+    Invoke-Expression -Command $expression -erroraction silentlycontinue
+}
+
+$vpn = Get-VpnConnection -Name $vpnName -erroraction silentlycontinue
+if ($vpn) { 
+    Disconnect-VPN
+    Remove-VpnConnection -Name $vpnName -Force -erroraction silentlycontinue 
+}
+
+# ? Setup VPN network
+Add-VpnConnection -RememberCredential -Name $vpnName -ServerAddress $vpnServerAddress -SplitTunneling
+    (Get-Content $env:USERPROFILE"\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk") -Replace 'IpInterfaceMetric=.*', 'IpInterfaceMetric=200' | Set-Content $env:USERPROFILE"\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk"
+    (Get-Content $env:USERPROFILE"\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk") -Replace 'Ipv6InterfaceMetric=.*', 'Ipv6InterfaceMetric=200' | Set-Content $env:USERPROFILE"\AppData\Roaming\Microsoft\Network\Connections\Pbk\rasphone.pbk"
+Connect-VPN
+
+if ($vpn.ConnectionStatus -eq "Disconnected") { Connect-VPN }
+
+Start-Sleep 5 
+Clear-Host
+
+Write-host ""
+Write-host "DONE!"
+Write-host ""
+Write-host "(--> After you finish playing press any key to close the VPN <--)"
+Write-host ""
+Write-host ""
+
+pause
+
+Disconnect-VPN
+Remove-VpnConnection -Name $vpnName -Force -erroraction silentlycontinue 
---
+++ route.bat
@@ -0,0 +1,37 @@
+@echo off
+:: BatchGotAdmin
+::-------------------------------------
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+ECHO Starting Valorant Voip Routing 
+set ip="IP Address"
+for /f "tokens=3 delims=: " %%I in ('netsh interface IPv4 show addresses "MajorAmariVPN" ^| findstr /C:%ip%') do set ip_address=%%I
+route add 3.0.0.0 mask 255.0.0.0 %ip_address%
+route add 8.0.0.0 mask 255.0.0.0 %ip_address%
+route add 18.0.0.0 mask 255.0.0.0 %ip_address%
+route add 34.0.0.0 mask 255.0.0.0 %ip_address%
+route add 35.0.0.0 mask 255.0.0.0 %ip_address%
+route add 50.0.0.0 mask 255.0.0.0 %ip_address%
+route add 52.0.0.0 mask 255.0.0.0 %ip_address%
+route add 54.0.0.0 mask 255.0.0.0 %ip_address%
+route add 216.0.0.0 mask 255.0.0.0 %ip_address%
+route add 74.0.0.0 mask 255.0.0.0 %ip_address%
+route add 63.0.0.0 mask 255.0.0.0 %ip_address%
